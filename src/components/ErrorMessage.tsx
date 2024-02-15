567bc1dd4730b9dd9568cdb34206476aed512759
Efraim Nabil <efraimefefraim@gmail.com>
2024-02-15 05:23:31 +0200

    Refactor ErrorMessage component to use a ternary operator for conditional rendering

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -3,13 +3,7 @@ interface IProps {
 }
 
 const ErrorMessage = ({msg}: IProps) => {
-  return (
-    msg ?
-        <span className="block text-red-700 font-semibold text-sm">
-            {msg}
-        </span>
-    : null
-  )
+  return msg ? <span className="block text-red-700 font-semibold text-sm"> {msg} </span> : null
 }
 
 export default ErrorMessage
